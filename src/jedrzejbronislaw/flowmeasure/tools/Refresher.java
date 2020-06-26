bf583663707795c34bd21305e4cbad11ef06c46b
JedrzejBronislaw <jedkru@wp.pl>
2020-06-26 13:13:57 +0200

    Extract LoopThread class

--- src/jedrzejbronislaw/flowmeasure/tools/Refresher.java
+++ src/jedrzejbronislaw/flowmeasure/tools/Refresher.java
@@ -1,46 +1,6 @@
 package jedrzejbronislaw.flowmeasure.tools;
 
-import javafx.application.Platform;
-
 public class Refresher {
-	
-	
-	private class LoopThread {
-		private final Thread t;
-		private boolean end = false;
-		private final int interval;
-		
-		public LoopThread(int interval, Runnable action) {
-			this.interval = interval;
-			
-			t = new Thread(() -> {
-				while(!end) {
-					Platform.runLater(action);
-					sleep();
-					}
-				});
-
-			t.setDaemon(true);
-			t.start();
-		}
-
-		public void end() {
-			end = true;
-		}
-
-		public boolean isAlive() {
-			return t.isAlive();
-		}
-
-		private void sleep() {
-			try {
-				Thread.sleep(interval);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
 	private final int interval;
 	private final Runnable refresh;
 	private LoopThread currentLoop = null;
