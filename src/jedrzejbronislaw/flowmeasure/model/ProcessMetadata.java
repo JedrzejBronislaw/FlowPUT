63c12b83d595b69e10374760bcddf96ecabe5b65
JedrzejBronislaw <jedkru@wp.pl>
2020-06-29 23:17:43 +0200

    Del raw metadata class
    and joint it with the main process metadata class

--- src/jedrzejbronislaw/flowmeasure/model/ProcessMetadata.java
+++ src/jedrzejbronislaw/flowmeasure/model/ProcessMetadata.java
@@ -2,19 +2,16 @@ package jedrzejbronislaw.flowmeasure.model;
 
 import java.time.LocalDateTime;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
 public class ProcessMetadata {
 
-	private ProcessMetadata_raw metadata = new ProcessMetadata_raw();
+	private String author;
+	private String name;
 	
-	public String getAuthor() {return metadata.author;}
-	public void setAuthor(String author) {metadata.author = author;}
-	
-	public String getName() {return metadata.name;}
-	public void setName(String name) {metadata.name = name;}
-	
-	public LocalDateTime getStartTime() {return metadata.startTime;}
-	public void setStartTime(LocalDateTime startTime) {metadata.startTime = startTime;}
-	
-	public LocalDateTime getEndTime() {return metadata.endTime;}
-	public void setEndTime(LocalDateTime endTime) {metadata.endTime = endTime;}
+	private LocalDateTime startTime;
+	private LocalDateTime endTime;
 }
