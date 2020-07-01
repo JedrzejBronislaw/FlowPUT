8331389fd71d69b0141b2122b83508fd21515f84
JedrzejBronislaw <jedkru@wp.pl>
2020-07-01 12:42:18 +0200

    Make formatter in FileName static final

--- src/jedrzejbronislaw/flowmeasure/FileNamer1.java
+++ src/jedrzejbronislaw/flowmeasure/FileNamer1.java
@@ -11,21 +11,22 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class FileNamer1 implements FileNamer {
 
+	private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH_mm_ss");
+	
 	@NonNull
 	private ProcessRepository process;
-	
-	private DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH_mm_ss");
 	
 	
 	@Override
 	public String createName() {
 
 		ProcessMetadata processMetadata = process.getMetadata();
+		
 		String processName = processMetadata.getName();
-		String authorName = processMetadata.getAuthor();
+		String authorName  = processMetadata.getAuthor();
 		String ifProcessName = processName.isEmpty() ? "" : " " + processName;
-		String ifAuthorName = authorName.isEmpty() ? "" : " (" +authorName + ")";
-		String initFileName = LocalDateTime.now().format(formatter) + ifProcessName + ifAuthorName;
+		String ifAuthorName  = authorName.isEmpty()  ? "" : " (" +authorName + ")";
+		String initFileName  = LocalDateTime.now().format(FORMATTER) + ifProcessName + ifAuthorName;
 
 		return initFileName;
 	}
