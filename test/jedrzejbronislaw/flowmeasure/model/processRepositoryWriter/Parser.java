d029ce815a3f7c60f69e657326c0af1da7fa7a39
JedrzejBronislaw <jedkru@wp.pl>
2020-07-03 16:54:28 +0200

    Add test for measurement in process writer

--- test/jedrzejbronislaw/flowmeasure/model/processRepositoryWriter/Parser.java
+++ test/jedrzejbronislaw/flowmeasure/model/processRepositoryWriter/Parser.java
@@ -1,6 +1,7 @@
 package jedrzejbronislaw.flowmeasure.model.processRepositoryWriter;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.regex.Pattern;
 
@@ -8,7 +9,19 @@ public class Parser {
 	
 	private final String content;
 
-	protected static List<String[]> metadata;
+	private List<String[]> metadata;
+	private List<String[]> dataHeader;
+	private List<String[]> data;
+	
+	public List<String[]> getMetadata() {
+		return Collections.unmodifiableList(metadata);
+	}
+	public List<String[]> getDataHeader() {
+		return Collections.unmodifiableList(dataHeader);
+	}
+	public List<String[]> getData() {
+		return Collections.unmodifiableList(data);
+	}
 	
 	public Parser(String content) {
 		this.content = content;
@@ -18,6 +31,8 @@ public class Parser {
 	private void parse() {
 		String[] lines = content.split(Pattern.quote(ProcessRepositoryCSVWriter.NEW_LINE_SEPARATOR));
 		metadata = extractMetadata(lines);
+		dataHeader = extractDataHeader(lines);
+		data = extractData(lines);
 	}
 	
 	private static List<String[]> extractMetadata(String[] lines) {
@@ -34,6 +49,41 @@ public class Parser {
 		}
 		
 		return metadata;
+	}
+	
+	private static List<String[]> extractDataHeader(String[] lines) {
+		List<String[]> dataheader = new ArrayList<>();
+		
+		for(int i=0; i<lines.length-2; i++) {
+			String line = lines[i];
+			
+			if(line.equals(ProcessRepositoryCSVWriter.DATA_HEAD)) {
+				dataheader.add(lines[i+1].split(ProcessRepositoryCSVWriter.SEPARATOR));
+				dataheader.add(lines[i+2].split(ProcessRepositoryCSVWriter.SEPARATOR));
+				break;
+			}
+		}
+		
+		return dataheader;
+	}
+	
+	private static List<String[]> extractData(String[] lines) {
+		List<String[]> data = new ArrayList<>();
+		boolean dataLine = false;
+		
+		for(int i=0; i<lines.length; i++) {
+			String line = lines[i];
+			
+			if(line.equals(ProcessRepositoryCSVWriter.DATA_HEAD)) {
+				dataLine = true;
+				i += 2;
+			} else
+			
+			if (dataLine)
+				data.add(line.split(ProcessRepositoryCSVWriter.SEPARATOR));
+		}
+		
+		return data;
 	}
 	
 	public boolean propertyExists(String propretyName) {
