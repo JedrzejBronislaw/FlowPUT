5b5defcf8898f8ea5d2e1c51e960318cc6e7c48c
JedrzejBronislaw <jedkru@wp.pl>
2020-07-07 22:36:41 +0200

    Refactor DataBuffer

--- src/jedrzejbronislaw/flowmeasure/services/DataBuffer.java
+++ src/jedrzejbronislaw/flowmeasure/services/DataBuffer.java
@@ -3,9 +3,7 @@ package jedrzejbronislaw.flowmeasure.services;
 import jedrzejbronislaw.flowmeasure.FlowMeasurementConsumer;
 
 public interface DataBuffer extends FlowMeasurementConsumer {
-
 	void newFlows(int[] flows);
-
 	void newFlow(int flow);
 
 	void setInterval(int interal);
--- src/jedrzejbronislaw/flowmeasure/services/DataBuffer1.java
+++ src/jedrzejbronislaw/flowmeasure/services/DataBuffer1.java
@@ -6,40 +6,38 @@ import java.time.temporal.ChronoUnit;
 import jedrzejbronislaw.flowmeasure.model.ProcessRepository;
 import lombok.Getter;
 import lombok.NonNull;
-import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 
-@RequiredArgsConstructor
 public class DataBuffer1 implements DataBuffer {
 
 	@NonNull
 	private ProcessRepository repository;
 	
-	@NonNull
-	@Setter
-	@Getter
+	@Setter @Getter
 	private int interval;
 	
-	private int pulseBuffer;
-	
+	private int pulseBuffer = 0;
 	private LocalDateTime lastTime = null;
+	
+	public DataBuffer1(ProcessRepository repository, int interval) {
+		this.repository = repository;
+		this.interval = interval;
+	}
 	
 	@Override
 	public void newFlows(int[] flows) {
-		
 		newFlow(flows[0]);
-		
 	}
 
 	@Override
 	public void newFlow(int flow) {
-		LocalDateTime now = LocalDateTime.now();
-		long period;
+		newFlow(LocalDateTime.now(), flow);
+	}
+	
+	public void newFlow(LocalDateTime time, int flow) {
 
 		if (lastTime == null) {
-			lastTime = now;
-			System.out.println("now:   " + now.toString());
-			System.out.println("start: " + repository.getMetadata().getStartTime());
+			firstTime(time);
 			return;
 		}
 		
@@ -51,34 +49,55 @@ public class DataBuffer1 implements DataBuffer {
 		
 		pulseBuffer += flow;
 
-		do {
-			period = ChronoUnit.MILLIS.between(lastTime, now);	
+		out(time);
+	}
 
-			if(period >= interval) {
-				float ratio = (float)interval/period;
-				
-				int portion = Math.round(pulseBuffer*ratio);
-				pulseBuffer -= portion;
-				
-				lastTime = lastTime.plus(interval, ChronoUnit.MILLIS);
-				out(LocalDateTime.from(lastTime), portion);
-			}
+	private void firstTime(LocalDateTime now) {
+		lastTime = now;
+		System.out.println("now:   " + now.toString());
+		System.out.println("start: " + repository.getMetadata().getStartTime());
+	}
+
+	private void out(LocalDateTime now) {
+		long period;
+		int portion;
+		
+		while((period = sinceLastTime(now)) >= interval) {
+			portion = takePortion(period);
 			
-		} while(period >= interval);
+			updateLastTime();
+			saveToRepository(LocalDateTime.from(lastTime), portion);
+		}
+	}
+
+	private void updateLastTime() {
+		lastTime = lastTime.plus(interval, ChronoUnit.MILLIS);
+	}
+
+	private long sinceLastTime(LocalDateTime now) {
+		return ChronoUnit.MILLIS.between(lastTime, now);
+	}
+
+	private int takePortion(long sinceLastTime) {
+		float ratio = (float)interval/sinceLastTime;
+		
+		int portion = Math.round(pulseBuffer*ratio);
+		pulseBuffer -= portion;
+		
+		return portion;
 	}
 	
-	public void out(LocalDateTime time, int flow) {
+	public void saveToRepository(LocalDateTime time, int flow) {
 		int[] tempFlows = new int[6];
 		
+		System.out.println("  " +
+			time.toString() + "\t" +
+			flow + "\t" +
+			(ChronoUnit.MILLIS.between(repository.getMetadata().getStartTime(), time)) + "\t" +
+			(ChronoUnit.MILLIS.between(repository.getMetadata().getStartTime(), time)/1000));
+		
 		tempFlows[0] = flow;
-		
-			System.out.println("  " +
-						time.toString() + "\t" +
-						flow + "\t" +
-						( ChronoUnit.MILLIS.between(repository.getMetadata().getStartTime(), time)) + "\t" +
-						( ChronoUnit.MILLIS.between(repository.getMetadata().getStartTime(), time)/1000));
 		repository.addFlowMeasurement(time, tempFlows);
-		
 	}
 	
 	@Override
