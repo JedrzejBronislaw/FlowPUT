43d05b1d4d643eff79148da75f2a5317a1b1f4f2
JedrzejBronislaw <jedkru@wp.pl>
2020-07-15 02:30:39 +0200

    Add run BiConsumer method in Injection class

--- src/jedrzejbronislaw/flowmeasure/tools/Injection.java
+++ src/jedrzejbronislaw/flowmeasure/tools/Injection.java
@@ -1,5 +1,6 @@
 package jedrzejbronislaw.flowmeasure.tools;
 
+import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
 
@@ -13,6 +14,11 @@ public class Injection {
 	public static <ArgType> void run(Consumer<ArgType> action, ArgType arg) {
 		if(action != null)
 			action.accept(arg);
+	}
+	
+	public static <ArgType1, ArgType2> void run(BiConsumer<ArgType1, ArgType2> action, ArgType1 arg1, ArgType2 arg2) {
+		if(action != null)
+			action.accept(arg1, arg2);
 	}
 
 	public static <ArgType> ArgType get(Supplier<ArgType> supplier, ArgType defaultValue) {
