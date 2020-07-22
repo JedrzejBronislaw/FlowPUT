d84559264e7b554c4bf16fe6739aba9879751968
JedrzejBronislaw <jedkru@wp.pl>
2020-07-22 15:33:59 +0200

    Extract UARTDevice class form FlowDevice class

--- src/jedrzejbronislaw/flowmeasure/flowDevice/FlowDevice.java
+++ src/jedrzejbronislaw/flowmeasure/flowDevice/FlowDevice.java
@@ -2,21 +2,14 @@ package jedrzejbronislaw.flowmeasure.flowDevice;
 
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
-import java.util.function.Function;
 import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import jedrzejbronislaw.flowmeasure.tools.Injection;
-import lombok.Getter;
 import lombok.Setter;
 
-public class FlowDevice {
+public class FlowDevice extends UARTDevice {
 
-	private enum MessageTag {
-		CORRECT, INCORRECT, IGNORED
-	}
-	
-	private static final String LINE_SEPARATOR_REGEX = "\\r?\\n";
 	private static final String VALUE_SEPARATOR = ";";
 	private static final char LINE_FIRST_CHAR   = '^';
 	private static final char LINE_LAST_CHAR    = '$';
@@ -24,66 +17,17 @@ public class FlowDevice {
 	private static final String PROOF_REQUEST = "FlowDevice";
 	private static final String PROOF_MESSAGE = "FD present!";
 
-	private IUART uart;
 
 	@Setter private BiConsumer<Integer, Integer> newSingleFlowReceive;
 	@Setter private Consumer<int[]> newFlowsReceive;
-	@Setter private Consumer<String> incorrectMessageReceive;
-	@Setter private Runnable deviceConfirmation;
-	@Setter private Function<UARTParams, IUART> uartGenerator = UART::new;
-	
-	@Getter private boolean connected     = false;
-	@Getter private boolean correctDevice = false;
-	
-	private boolean connecting = false;
 	
 
-	public FlowDevice() {}
-	public FlowDevice(Function<UARTParams, IUART> uartGenerator) {
-		if (uartGenerator != null) this.uartGenerator = uartGenerator;
-	}
-	
-	
-	public boolean connect(UARTParams params) {
-		if(connecting) return false;
-		connecting = true;
-		
-		uart = uartGenerator.apply(params);
-		uart.setReceiveMessage(message -> handleMessage(message));
-		connected = uart.connect();
-		
-		connecting = false;
-		
-		return connected;
+	public FlowDevice() {
+		super(PROOF_REQUEST, PROOF_MESSAGE);
 	}
 
-	public void disconnect() {
-		if(uart == null || !uart.isPortOpen()) return;
-		
-		uart.disconnect();
-		connected = false;
-		correctDevice = false;
-	}
-
-	public void sendProofRequest() {
-		uart.send(PROOF_REQUEST);
-	}
-
-	private void handleMessage(String messageLines) {
-		if(messageLines == null || messageLines.isEmpty()) return;
-		
-		String[] messages = messageLines.split(LINE_SEPARATOR_REGEX);
-		
-		Stream.of(messages).filter(this::unexecuted).forEach(message -> Injection.run(incorrectMessageReceive, message));
-	}
-	
-	private boolean unexecuted(String message) {
-		return handleMessageLine(message) == MessageTag.INCORRECT;
-	}
-
-	private MessageTag handleMessageLine(String message) {
-		if(isProofMessage(message))     return MessageTag.CORRECT;
-		if(!correctDevice)              return MessageTag.IGNORED;
+	@Override
+	protected MessageTag handleMessageLine(String message) {
 		if(!correctLineFormat(message)) return MessageTag.INCORRECT;
 
 		String content = extraxtContent(message);
@@ -134,15 +78,5 @@ public class FlowDevice {
 		return length >= 4
 			&& message.charAt(0) == LINE_FIRST_CHAR
 			&& message.charAt(length-1) == LINE_LAST_CHAR;
-		
-	}
-
-	private boolean isProofMessage(String message) {
-		if (!message.equals(PROOF_MESSAGE)) return false;
-		
-		correctDevice = true;
-		Injection.run(deviceConfirmation);
-		
-		return true;
 	}
 }
---
+++ src/jedrzejbronislaw/flowmeasure/flowDevice/UARTDevice.java
@@ -0,0 +1,94 @@
+package jedrzejbronislaw.flowmeasure.flowDevice;
+
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Stream;
+
+import jedrzejbronislaw.flowmeasure.tools.Injection;
+import lombok.Getter;
+import lombok.Setter;
+
+public abstract class UARTDevice {
+
+	enum MessageTag {
+		CORRECT, INCORRECT, IGNORED
+	}
+	
+	private static final String LINE_SEPARATOR_REGEX = "\\r?\\n";
+	
+	protected final String PROOF_REQUEST;
+	protected final String PROOF_MESSAGE;
+
+	private IUART uart;
+
+	@Setter private Consumer<String> incorrectMessageReceive;
+	@Setter private Runnable deviceConfirmation;
+	@Setter private Function<UARTParams, IUART> uartGenerator = UART::new;
+	
+	@Getter private boolean connected     = false;
+	@Getter private boolean correctDevice = false;
+	
+	private boolean connecting = false;
+
+	protected abstract MessageTag handleMessageLine(String message);
+	
+	
+	public UARTDevice(String proofRequest, String proofMessage) {
+		PROOF_REQUEST = proofRequest;
+		PROOF_MESSAGE = proofMessage;
+	}
+
+
+	public boolean connect(UARTParams params) {
+		if(connecting) return false;
+		connecting = true;
+		
+		uart = uartGenerator.apply(params);
+		uart.setReceiveMessage(message -> handleMessage(message));
+		connected = uart.connect();
+		
+		connecting = false;
+		
+		return connected;
+	}
+
+	public void disconnect() {
+		if(uart == null || !uart.isPortOpen()) return;
+		
+		uart.disconnect();
+		connected = false;
+		correctDevice = false;
+	}
+
+	public void sendProofRequest() {
+		uart.send(PROOF_REQUEST);
+	}
+
+	private void handleMessage(String messageLines) {
+		if(messageLines == null || messageLines.isEmpty()) return;
+		
+		String[] messages = messageLines.split(LINE_SEPARATOR_REGEX);
+		
+		Stream.of(messages).filter(this::unexecuted).forEach(message -> Injection.run(incorrectMessageReceive, message));
+	}
+	
+	private boolean unexecuted(String message) {
+		return handleLine(message) == MessageTag.INCORRECT;
+	}
+
+	private MessageTag handleLine(String message) {
+		if(isProofMessage(message))     return MessageTag.CORRECT;
+		if(!correctDevice)              return MessageTag.IGNORED;
+		
+		return handleMessageLine(message);
+	}
+
+	private boolean isProofMessage(String message) {
+		if (!message.equals(PROOF_MESSAGE)) return false;
+		
+		correctDevice = true;
+		Injection.run(deviceConfirmation);
+		
+		return true;
+	}
+}
