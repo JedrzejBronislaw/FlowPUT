a57c343bacc4ac7cce30873b7e1f5f4d2881d36a
JedrzejBronislaw <jedkru@wp.pl>
2020-07-28 17:41:03 +0200

    Add reset button in LivePane

--- src/jedrzejbronislaw/flowmeasure/view/live/LivePaneBuilder.java
+++ src/jedrzejbronislaw/flowmeasure/view/live/LivePaneBuilder.java
@@ -1,11 +1,15 @@
 package jedrzejbronislaw.flowmeasure.view.live;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javafx.scene.Node;
 import jedrzejbronislaw.flowmeasure.components.flowConverter.FlowConverters;
 import jedrzejbronislaw.flowmeasure.settings.Consts;
 import jedrzejbronislaw.flowmeasure.view.Builder;
 import jedrzejbronislaw.flowmeasure.view.ViewMediator;
 import jedrzejbronislaw.flowmeasure.view.live.flowPreview.FlowPreviewBuilder;
+import jedrzejbronislaw.flowmeasure.view.live.flowPreview.FlowPreviewController;
 import lombok.Getter;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
@@ -18,8 +22,13 @@ public class LivePaneBuilder extends Builder<LivePaneController> {
 	@NonNull private final ViewMediator viewMediator;
 	@NonNull private final FlowConverters flowConverters;
 	
+	private List<FlowPreviewController> flowsPreviews = new ArrayList<>(Consts.FLOWMETERS_NUMBER);
+	
+	
 	@Override
 	protected void afterBuild() {
+		
+		controller.setResetAction(this::resetVolume);
 		
 		for(int i=0; i<Consts.FLOWMETERS_NUMBER; i++)
 			controller.addFlowPreview(flowPreview(i));
@@ -29,8 +38,13 @@ public class LivePaneBuilder extends Builder<LivePaneController> {
 		FlowPreviewBuilder builder = new FlowPreviewBuilder(number, flowConverters.get(number));
 		builder.build();
 		
+		flowsPreviews.add(builder.getController());
 		viewMediator.setFlowPreviewer(number, builder.getController()::addPulses);
 		
 		return builder.getNode();
+	}
+	
+	private void resetVolume() {
+		flowsPreviews.forEach(flow -> flow.resetSecPulse());
 	}
 }
--- src/jedrzejbronislaw/flowmeasure/view/live/LivePaneController.java
+++ src/jedrzejbronislaw/flowmeasure/view/live/LivePaneController.java
@@ -6,11 +6,17 @@ import java.util.ResourceBundle;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.Node;
+import javafx.scene.control.Button;
 import javafx.scene.layout.VBox;
+import jedrzejbronislaw.flowmeasure.tools.Injection;
+import lombok.Setter;
 
 public class LivePaneController implements Initializable {
 
 	@FXML private VBox flowBox;
+	@FXML private Button resetButton;
+	
+	@Setter private Runnable resetAction;
 
 	
 	public void addFlowPreview(Node node) {
@@ -18,5 +24,7 @@ public class LivePaneController implements Initializable {
 	}
 	
 	@Override
-	public void initialize(URL arg0, ResourceBundle arg1) {}
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		resetButton.setOnAction(e -> Injection.run(resetAction));
+	}
 }
